6c7b210f75258d12fc592b4ae3dc3f3a8e3e9a81
Taha Loghmari <157913779+TahaLoghmari@users.noreply.github.com>
2024-02-11 08:56:42 +0100

    C. Fadi and LCM.cpp

---
+++ C. Fadi and LCM.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define IWillWinTCPC cout.tie(0), cin.tie(0), ios_base::sync_with_stdio(0); 
+#define Take(x) for ( auto & d : x ) cin >> d ; 
+#define No cout << "No" << endl  
+#define Yes cout << "Yes" << endl  
+#define Sort(x) sort( x.begin() , x.end() ) ;
+#define RSort(x) sort( x.begin() , x.end() ) ; reverse ( x.begin() , x.end() ) ; 
+#define MOD 998244353
+#define Sum(A) accumulate( A.begin() , A.end() , (int)0 ) 
+#define MAX LONG_LONG_MAX 
+#define MIN LONG_LONG_MIN 
+#define endl '\n' 
+#define isSorted(x) is_sorted(A.begin(),A.end())
+#define ll long long 
+/* 
+ASK WHY???!!! DOES IT MATTER IF.... ??!! ARE WE SUPPOSED TO DO LIKE HE SAID???!! DOES IT MATTER THAT THE SUBPROBLEMS ARE SEPERATED ? AM I SUPPOSED TO FIND THE OPTIMAL WAY BY MYSELF ? WHY DID HE GAVE ME SUCH CONSTRAINT ? CONSIDER ELIMINATING SOME CASSES OR EXCESS ELEMENTS 
+*/
+/* 
+STAY ORGANIZED , DON'T RUSH , DON'T THINK ABOUT A SOLUTION THAT's BASED ON LUCK , DON'T CHECK WRONG ANSWERS  
+*/
+/* IF YOU'RE ASKED FOR MIN OR MAX WHAT WILL YOU DO??? --->>> BINARY SEARCH ON ANSWERS ( MOST OF THE TIME )*/
+void solve()
+{
+    ll x ; cin >> x ; 
+    ll Min = MAX , a = 0 , b = 0;
+    if ( x == 1 ) 
+    {
+        cout << 1 << " " << 1 << endl ; return;
+    }
+    for ( ll i = 1 ; i * i < x ; ++i )
+    {
+        if ( x % i == 0 ) 
+        {
+            if ( Min > max ( i , x / i ) && lcm ( i , x/i ) == x ) 
+            {
+                Min = max ( i , x/i )  ;
+                a = i ; b = x / i ; 
+            }   
+            
+        }
+    }
+    cout << a << " " << b << endl ; 
+} 
+int main() 
+{
+    #ifndef ONLINE_JUDGE
+    freopen("Input.txt", "r", stdin);
+    freopen("Output.txt", "w", stdout);
+    #endif
+    IWillWinTCPC ;   
+    cout<<fixed<<setprecision(14);
+    int t = 1 ; 
+    // cin >> t ; 
+    while ( t-- ) solve();
+    return 0;
+}
