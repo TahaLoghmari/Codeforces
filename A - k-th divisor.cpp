3507b7fa38898c009e42da1d8406bb057fb33aa4
Taha Loghmari <157913779+TahaLoghmari@users.noreply.github.com>
2024-02-15 14:33:26 +0100

    Create A - k-th divisor.cpp

---
+++ A - k-th divisor.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define IWillWinTCPC cout.tie(0), cin.tie(0), ios_base::sync_with_stdio(0); 
+#define Take(x) for ( auto & d : x ) cin >> d ; 
+#define No cout << "No" << endl  
+#define Yes cout << "Yes" << endl  
+#define Sort(x) sort( x.begin() , x.end() ) ;
+#define RSort(x) sort( x.begin() , x.end() ) ; reverse ( x.begin() , x.end() ) ; 
+#define MOD 998244353
+#define Sum(A) accumulate( A.begin() , A.end() , (int)0 ) 
+#define MAX LONG_LONG_MAX 
+#define MIN LONG_LONG_MIN 
+#define endl '\n' 
+#define isSorted(x) is_sorted(A.begin(),A.end())
+#define Unique(x) x.erase(unique(x.begin(),x.end()),x.end());
+#define pb push_back 
+#define ll long long 
+/* 
+ASK WHY???!!! DOES IT MATTER IF.... ??!! ARE WE SUPPOSED TO DO LIKE HE SAID???!! DOES IT MATTER THAT THE SUBPROBLEMS ARE SEPERATED ? AM I SUPPOSED TO FIND THE OPTIMAL WAY BY MYSELF ? WHY DID HE GAVE ME SUCH CONSTRAINT ? CONSIDER ELIMINATING SOME CASSES OR EXCESS ELEMENTS 
+*/
+/* 
+STAY ORGANIZED , DON'T RUSH , DON'T THINK ABOUT A SOLUTION THAT's BASED ON LUCK , DON'T CHECK WRONG ANSWERS  
+*/
+void solve()
+{
+    ll n , k ; cin >> n >> k ; 
+    vector<ll> Divisors ; 
+    ll Count = 0 ; 
+    for ( int i = 1 ; i <= sqrt(n) ; ++i ) 
+    {
+        if ( n  % i == 0 )
+        {
+            Count++;
+            if ( n/i != i ) Divisors.push_back( n / i ) ; 
+            if ( Count == k ) 
+            {
+                cout << i << endl ; return;
+            }
+        } 
+    }
+    Sort(Divisors);
+    for ( int i = 0 ; i < Divisors.size() ; ++i ) 
+    {
+        Count++;
+        if ( Count == k ) 
+        {
+            cout << Divisors[i] << endl ; return;
+        }
+    }
+    cout << -1 << endl ; 
+}
+signed main() 
+{
+    #ifndef ONLINE_JUDGE
+    freopen("Input.txt", "r", stdin);
+    freopen("Output.txt", "w", stdout);
+    #endif
+    IWillWinTCPC ;   
+    cout<<fixed<<setprecision(14);
+    int t = 1 ; 
+    // cin >> t ; 
+    while ( t-- ) solve();
+    return 0;
+}
