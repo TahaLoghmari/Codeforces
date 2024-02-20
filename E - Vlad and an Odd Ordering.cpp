7b3651172abf7b0697d4cf38547108131270f4c3
Taha Loghmari <157913779+TahaLoghmari@users.noreply.github.com>
2024-02-20 16:38:40 +0100

    E - Vlad and an Odd Ordering.cpp

---
+++ E - Vlad and an Odd Ordering.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define IWillWinTCPC cout.tie(0), cin.tie(0), ios_base::sync_with_stdio(0);
+#define ll long long 
+#define Take(x) for ( auto & d : x ) cin >> d ; 
+#define No cout << "No" << endl  
+#define Yes cout << "Yes" << endl  
+#define Sort(x) sort( x.begin() , x.end() ) ;
+#define RSort(x) sort( x.begin() , x.end() ) ; reverse ( x.begin() , x.end() ) ; 
+#define MOD 998244353
+#define Sum(A) accumulate( A.begin() , A.end() , (int)0 ) 
+#define MAX LONG_LONG_MAX 
+#define MIN LONG_LONG_MIN 
+#define endl '\n' 
+#define isSorted(x) is_sorted(A.begin(),A.end())
+#define Unique(x) x.erase(unique(x.begin(),x.end()),x.end());
+void solve()
+{
+    ll n , k ; cin >> n >> k ; 
+    for ( ll i = 1 ; i <= log2(n)+1 ; ++i ) 
+    {
+        ll gap = pow(2,i) ; 
+        ll Plus = gap / 2 ; 
+        if ( (( n + Plus )/gap) < k )
+        {
+            k -= ( n + Plus )/gap ; continue;
+        }
+        cout << (ll)(Plus + ( gap * (k-1) )) << endl ; return; 
+    }
+} 
+signed main() 
+{
+    #ifndef ONLINE_JUDGE
+    freopen("Input.txt", "r", stdin);
+    freopen("Output.txt", "w", stdout);
+    #endif
+    IWillWinTCPC ;   
+    cout<<fixed<<setprecision(14);
+    int t = 1 ; 
+    cin >> t ; 
+    while ( t-- ) solve();
+    return 0;
+}
