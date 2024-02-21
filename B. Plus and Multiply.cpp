a450f2ec669c958b76fb0167e491600473892bd3
Taha Loghmari <157913779+TahaLoghmari@users.noreply.github.com>
2024-02-21 19:18:17 +0100

    B. Plus and Multiply.cpp

---
+++ B. Plus and Multiply.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define IWillWinTCPC cout.tie(0), cin.tie(0), ios_base::sync_with_stdio(0);
+#define ll long long 
+#define Take(x) for ( auto & d : x ) cin >> d ; 
+#define No cout << "No" << endl  
+#define Yes cout << "Yes" << endl  
+#define Sort(x) sort( x.begin() , x.end() ) ;
+#define RSort(x) sort( x.begin() , x.end() ) ; reverse ( x.begin() , x.end() ) ; 
+#define MOD 998244353
+#define Sum(A) accumulate( A.begin() , A.end() , (int)0 ) 
+#define MAX LONG_LONG_MAX 
+#define MIN LONG_LONG_MIN 
+#define endl '\n' 
+#define isSorted(x) is_sorted(A.begin(),A.end())
+#define Unique(x) x.erase(unique(x.begin(),x.end()),x.end());
+void solve()
+{
+    ll n , a , b , ToPow = 0 ; cin >> n >> a >> b ; 
+    if ( a == 1 ) 
+    {
+        if ( (n - 1) % b != 0 ) No ; 
+        else Yes ; 
+        return;
+    }
+    while ( pow( a , ToPow) <= n ) 
+    {
+        if ( (n - (int)pow(a,ToPow)) % b == 0 ) { Yes ; return ; }
+        ++ToPow;
+    }
+    No ; 
+} 
+signed main() 
+{
+    #ifndef ONLINE_JUDGE
+    freopen("Input.txt", "r", stdin);
+    freopen("Output.txt", "w", stdout);
+    #endif
+    IWillWinTCPC ;   
+    cout<<fixed<<setprecision(14);
+    int t = 1 ; 
+    cin >> t ; 
+    while ( t-- ) solve();
+    return 0;
+}
