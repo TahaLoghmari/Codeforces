138f5cc3619097bc24dc9ba7b078d51cfaa720ff
Taha Loghmari <157913779+TahaLoghmari@users.noreply.github.com>
2024-02-28 15:34:12 +0100

    A. Music.cpp

---
+++ A. Music.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define IWillWinTCPC cout.tie(0), cin.tie(0), ios_base::sync_with_stdio(0);
+#define ll long long 
+#define Take(x) for ( auto & d : x ) cin >> d ; 
+#define No cout << "No" << endl  
+#define Yes cout << "Yes" << endl  
+#define Sort(x) sort( x.begin() , x.end() ) ;
+#define RSort(x) sort( x.begin() , x.end() ) ; reverse ( x.begin() , x.end() ) ; 
+#define MOD 998244353
+#define Sum(A) accumulate( A.begin() , A.end() , (int)0 ) 
+#define MAX LONG_LONG_MAX 
+#define MIN LONG_LONG_MIN 
+#define endl '\n' 
+#define isSorted(x) is_sorted(A.begin(),A.end())
+#define Unique(x) x.erase(unique(x.begin(),x.end()),x.end());
+class Solution
+{
+public:
+
+void solve()
+{
+    int T , S , Q ; cin >> T >> S >> Q ; 
+    int Downloading = S , Listening = 0 , Count = 1 ; 
+    while ( Downloading < T ) 
+    {
+        Downloading += (Q-1) ; 
+        Listening += Q ; 
+        if ( Listening >= Downloading && Downloading < T ) 
+        {
+            Listening = 0 ; 
+            Count++;
+        }
+    } 
+    cout << Count << endl ; 
+}
+};
+signed main() 
+{
+    #ifndef ONLINE_JUDGE
+    freopen("Input.txt", "r", stdin);
+    freopen("Output.txt", "w", stdout);
+    #endif
+    IWillWinTCPC ;   
+    cout<<fixed<<setprecision(14);
+    int t = 1 ; 
+    // cin >> t ; 
+    while ( t-- ){ Solution Sol ; Sol.solve(); } 
+    return 0;
+}
