1bba8683f0404c9de22cd6470cb4587471b891ac
Taha Loghmari <157913779+TahaLoghmari@users.noreply.github.com>
2024-02-28 18:15:39 +0100

    D1. Balance (Easy version).cpp

---
+++ D1. Balance (Easy version).cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define IWillWinTCPC cout.tie(0), cin.tie(0), ios_base::sync_with_stdio(0);
+#define ll long long 
+#define Take(x) for ( auto & d : x ) cin >> d ; 
+#define No cout << "No" << endl  
+#define Yes cout << "Yes" << endl  
+#define Sort(x) sort( x.begin() , x.end() ) ;
+#define RSort(x) sort( x.begin() , x.end() ) ; reverse ( x.begin() , x.end() ) ; 
+#define MOD 998244353
+#define Sum(A) accumulate( A.begin() , A.end() , (int)0 ) 
+#define MAX LONG_LONG_MAX 
+#define MIN LONG_LONG_MIN 
+#define endl '\n' 
+#define isSorted(x) is_sorted(A.begin(),A.end())
+#define Unique(x) x.erase(unique(x.begin(),x.end()),x.end());
+class Solution
+{
+public:
+
+void solve()
+{
+    int q ; cin >> q ;
+    map<ll,ll> Occ ;
+    map<ll,ll> Left ; 
+    for ( int i = 0 ; i < q ; ++i ) 
+    {
+        char O ; ll x ; cin >> O >> x ; 
+        if ( O == '+' ) Occ[x]++;
+        else
+        {
+            ll Start = (Left[x] != 0 ? Left[x] : x ); 
+            while ( Occ[Start] != 0 ) Start += x ;
+            Left[x] = Start ;  
+            cout << Start << endl ; 
+        }
+    }
+}
+};
+signed main() 
+{
+    #ifndef ONLINE_JUDGE
+    freopen("Input.txt", "r", stdin);
+    freopen("Output.txt", "w", stdout);
+    #endif
+    IWillWinTCPC ;   
+    cout<<fixed<<setprecision(14);
+    int t = 1 ; 
+    // cin >> t ; 
+    while ( t-- ){ Solution Sol ; Sol.solve(); } 
+    return 0;
+}
